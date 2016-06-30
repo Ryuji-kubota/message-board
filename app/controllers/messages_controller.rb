952fdeb6b4440835a6db67bbb9307b3899441af9
Kubota <xyz58344981@yahoo.co.jp>
2016-06-30 02:58:11 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+     @message = Message.new
+     # Messageを全て取得する。
+     @message = Message.all
   end
+  
+   def index
+     @message = Message.new
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
+
