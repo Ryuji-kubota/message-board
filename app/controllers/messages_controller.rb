ee979ae2f2ad2f931706a1f4ae949ea12ad98031
Kubota <xyz58344981@yahoo.co.jp>
2016-07-02 08:24:30 +0000

    kadai r1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,40 +11,40 @@ class MessagesController < ApplicationController
   end
   
   def update
-  if @message.update(message_params)
-    # 保存に成功した場合はトップページへリダイレクト
+   if @message.update(message_params)
+     # 保存に成功した場合はトップページへリダイレクト
      redirect_to root_path , notice: 'メッセージを編集しました'
-   else
-    # 保存に失敗した場合は編集画面へ戻す
+     else
+     # 保存に失敗した場合は編集画面へ戻す
     　render 'edit'
-    　end
-    end
+   end
+  end
   
   def create
     @message = Message.new(message_params)
-  if @message.save
+   if @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
-  else
+    else
     # メッセージが保存できなかった時
      @messages = Message.all
      flash.now[:alert] = "メッセージの保存に失敗しました。"
      render 'index'
+   end
   end
-end
+
+ def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+ end
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   ## ここまで
-end
 
-  def destroy
-    @message.destroy
-    redirect_to root_path, notice: 'メッセージを削除しました'
-  end
 
   def set_message
-   @message = Message.find(params[:id])
+    @message = Message.find(params[:id])
   end
 end
